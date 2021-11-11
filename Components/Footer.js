9e65215cafa8c56314ff93ea6d3ef7d9de94bffb
Malkeet <0786malkeetsingh@gmail.com>
2021-11-11 10:19:19 +0530

    Airbnb clone

---
+++ Components/Footer.js
@@ -0,0 +1,38 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600"> 
+                <div className="space-y-4 text-xs text-gray-800">
+                     <h5 className="font-bold">ABOUT</h5>
+                    <p>How Airbnb Works</p>
+                    <p>Newsroom</p>
+                    <p>Investor</p>
+                    <p>Airbnb Plus</p>
+                    <p>Airbnb Lexue</p>
+                </div>
+                
+                <div className="space-y-4 text-xs text-gray-800">
+                     <h5 className="font-bold">COMMUNITY</h5>
+                    <p>Accessibility</p>
+                    <p>This is not Real site</p>
+                    <p>Its a pretty Awesome Clone</p>
+                    <p>Refferal</p>
+                </div>
+                <div className="space-y-4 text-xs text-gray-800">
+                     <h5 className="font-bold">HOST</h5>
+                    <p>MALKEET</p>
+                    <p>Presents</p>
+                    <p>Investor</p>
+                </div>
+                <div className="space-y-4 text-xs text-gray-800">
+                     <h5 className="font-bold">SUPPORT</h5>
+                    <p>Help Centre</p>
+                    <p>Trust & Safety</p>
+                    <p>Say Hi Youtube</p>
+                    <p>Easter Eggs</p>
+                    <p>For the Win</p>
+                </div>
+        </div>
+    )
+}
+
+export default Footer
